1031629c81a3a1a32cf6d8ade146ed604dd095f3
Zahir Ali <zahirali@Zahirs-MacBook-Air.local>
2023-03-16 17:48:33 -0700

    added pages/comps

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import {  Box, Container, Heading } from "@chakra-ui/react"
 const Page = () => {
     return (
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I'm a Designer/Developer based in Vancouver, BC
+            </Box>
+
+            <Box display={{md:'flex'}}>
+            <Box flexGrow={1}>
+            <Heading as="h2" variant={"page-title"}>
+                Zahir Ali
+            </Heading>
+        <p>Designer/Deveolper</p>
+            </Box>
+            </Box>
+        </Container>
+        
     )
 }
 
