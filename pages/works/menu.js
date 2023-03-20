4eda029054e6f13d9e702ebc253dc47f93b02257
Zahir Ali <zahirali@Zahirs-MacBook-Air.local>
2023-03-19 19:52:15 -0700

    commit

--- pages/works/menu.js
+++ pages/works/menu.js
@@ -1,12 +1,9 @@
 import {
     Container,
     Badge,
-    Link,
     List,
     ListItem,
-    AspectRatio
   } from '@chakra-ui/react'
-  import { ExternalLinkIcon } from '@chakra-ui/icons'
   import { Title, WorkImage, Meta } from '../../components/work'
   import P from '../../components/paragraph'
   import Layout from "../../components/layouts/article"
